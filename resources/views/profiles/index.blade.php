bfacb0da4387e73cf260544091b38224162e7b11
cosmic-onion <marmaladeking@gmail.com>
2020-05-19 21:46:20 +0100

    Follow button toggle

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,19 +4,24 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-        <img src="{{$user->profile->profileImage()}}" class="rounded-circle w-100">
+            <img src="{{$user->profile->profileImage()}}" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{$user->username}}</h1>
-                 @can ('update', $user->profile)
-                    <a href="/p/create">Add new post</a>
+                <div class="d-flex">
+                    <h1 class="align-items-center">{{$user->username}}</h1>
+
+                    <follow-button  user_id="{{ $user->id }}" follows="{{$follows}}"></follow-button>
+                </div>
+
+                @can ('update', $user->profile)
+                <a href="/p/create">Add new post</a>
                 @endcan
-                
+
             </div>
 
             @can ('update', $user->profile)
-                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
             @endcan
 
             <div class="d-flex">
@@ -24,7 +29,7 @@
                 <div class="pr-5"><strong>25k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold" >{{$user->profile->title}}</div>
+            <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
             <div>{{$user->profile->description}}</div>
             <div><a href="#">{{$user->profile->url}}</a></div>
         </div>
@@ -34,13 +39,13 @@
         @foreach($user->posts as $post)
 
         <div class="col-4 pb-4">
-           
+
             <a href="/p/{{$post->id}}">
-                 <img src="/storage/{{ $post->image }}" alt="" class="w-100">
+                <img src="/storage/{{ $post->image }}" alt="" class="w-100">
             </a>
         </div>
         @endforeach
-       
+
     </div>
 
 </div>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
+// Route::post('follow/{user}', function(){
+//    return['success'];
+// });
+
+Route::post('/follow/{user}','FollowsController@store');
+
+
 Route::get('/p/create', 'PostsController@create');
 Route::get('/p/{post}', 'PostsController@show');
 
